f105e7fe95ff40b72738c97ee4dbbcffacf9ea05
mantasreika <mantas.reika@presentconnection.eu>
2022-09-18 11:33:45 +0300

    added text view and buttons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+//    private tvMain: TextView;
+private lateinit var textView : TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
     }
+
+    fun onButtonClick(view: View) {
+        System.out.println("onButtonClick");
+        textView.text = "BOOM!"
+    }
+
+    fun onSecondButtonClick(view: View) {
+        System.out.println("onSecondButtonClick");
+        textView.setTextColor(Color.GREEN);
+    }
+
+
+//    fun onButtonClick() {
+//    }
 }
